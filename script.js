e7d31e5eb0826444254c80076ede8897567a0d51
Alex <psstudio.info@gmail.com>
2023-03-21 10:00:03 +0300

    Рефакторинг кода с использованием циклов

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -10,12 +10,25 @@ const personalMovieDB = {
     privat: false
 };
 
-const answer_1 = prompt('Один из последних просмотренных фильмов?', ''),
-      answer_2 = prompt('На сколько оцените его?', ''),
-      answer_3 = prompt('Один из последних просмотренных фильмов?', ''),
-      answer_4 = prompt('На сколько оцените его?', '');
+for (let i = 0; i < 2; i++) {
+    let answer_1 = prompt('Один из последних просмотренных фильмов?', ''),
+        answer_2 = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[answer_1] = answer_2;
-personalMovieDB.movies[answer_3] = answer_4;
+    if (answer_1 == '' || answer_1 == null || answer_2 == '' || answer_2 == null || answer_1.length > 50) {
+            i--;
+    } else {
+        personalMovieDB.movies[answer_1] = answer_2;
+    }
+}
+
+if (personalMovieDB.count < 10) {
+    console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+    console.log("Вы классический зритель");
+} else if (personalMovieDB.count > 30) {
+    console.log("Вы киноман");
+} else {
+    console.log("Произошла ошибка");
+}
 
 console.log(personalMovieDB);
